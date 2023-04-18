24626270af35dd08383f94c50a62929c53383dcc
Zacharia Ria Ndungu <zachariarian@gmail.com>
2023-04-18 21:12:12 +0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///loans.db'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
